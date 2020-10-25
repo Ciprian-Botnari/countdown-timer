9cf662f252a52a0d58c4336440138a5c1e1fb81b
Ciprian <botnari_ciprian@mail.ru>
2020-10-25 09:01:37 +0200

    Testing Git

--- js/main.js
+++ js/main.js
@@ -1,11 +1,2 @@
 let submitBtn = document.getElementById( 'submitBtn' );
-submitBtn.addEventListener( 'click', submitClick );
-
-function submitClick(e) 
-{
-  e.preventDefault();
-
-  let userInput = document.getElementById( 'userInput' ).value;
-  console.log( userInput );
-}
 
